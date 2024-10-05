3010c8a01ed42684e6c78d04827e557b5e1249d1
edenzewdu <edenzewdu434@gmail.com>
2024-10-05 05:29:53 +0300

    more sections on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 <div class="container text-center my-5">
     <h1 class="display-4 text-primary">Kuraz Enabra</h1>
     <p class="lead text-secondary">Shining for someone else, like a lamp</p>
-    <img src="{{ asset('image/Kindergarden-Kids-768x496.jpg') }}" alt="Fundraising for Schools" class="img-fluid rounded shadow-lg">
+    <img src="{{ asset('image/OIP.jpeg') }}" alt="Fundraising for Schools" class="img-fluid rounded shadow-lg">
     <a href="#about" class="btn btn-success btn-lg mt-4">Learn More</a>
 </div>
 
@@ -44,6 +44,75 @@
         <h2 class="text-center">About Kuraz Enabra</h2>
         <p class="lead text-center">Our mission is to empower high school students across Ethiopia by raising funds for educational resources, facilities, and scholarships.</p>
         <p class="text-center">We believe that every student deserves a bright future, and we are here to help them shine.</p>
+    </div>
+</div>
+
+<!-- How You Can Help Section -->
+<div id="help" class="help py-5">
+    <div class="container text-center">
+        <h2 class="text-primary">How You Can Help</h2>
+        <p class="lead text-secondary">Your support can make a huge difference in the lives of many students.</p>
+        <div class="row">
+            <div class="col-md-4">
+                <h4>Donate</h4>
+                <p>Contribute financially to provide educational resources and scholarships for students.</p>
+                <a href="#" class="btn btn-success">Donate Now</a>
+            </div>
+            <div class="col-md-4">
+                <h4>Volunteer</h4>
+                <p>Join us in our mission by volunteering your time and skills to help organize events or mentor students.</p>
+                <a href="#" class="btn btn-primary">Get Involved</a>
+            </div>
+            <div class="col-md-4">
+                <h4>Partner</h4>
+                <p>We collaborate with businesses and organizations to expand our impact and reach more schools.</p>
+                <a href="#" class="btn btn-info">Become a Partner</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Our Impact Section -->
+<div id="impact" class="impact bg-light py-5">
+    <div class="container">
+        <h2 class="text-center text-success">Our Impact</h2>
+        <p class="lead text-center">Together, we’ve made a lasting difference in the lives of students across Ethiopia.</p>
+        <div class="row text-center">
+            <div class="col-md-4">
+                <h3 class="text-primary">10,000+</h3>
+                <p>Students supported with educational resources</p>
+            </div>
+            <div class="col-md-4">
+                <h3 class="text-primary">500+</h3>
+                <p>Schools benefitted from facility upgrades</p>
+            </div>
+            <div class="col-md-4">
+                <h3 class="text-primary">150+</h3>
+                <p>Scholarships awarded to deserving students</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Partners Section -->
+<div id="partners" class="partners py-5 bg-secondary text-white">
+    <div class="container">
+        <h2 class="text-center">Our Partners</h2>
+        <p class="lead text-center">We are grateful for the support of our corporate and community partners.</p>
+        <div class="row text-center">
+            <div class="col-md-3">
+                <img src="{{ asset('image/partner1.png') }}" alt="Partner 1" class="img-fluid mb-3">
+            </div>
+            <div class="col-md-3">
+                <img src="{{ asset('image/partner2.png') }}" alt="Partner 2" class="img-fluid mb-3">
+            </div>
+            <div class="col-md-3">
+                <img src="{{ asset('image/partner3.png') }}" alt="Partner 3" class="img-fluid mb-3">
+            </div>
+            <div class="col-md-3">
+                <img src="{{ asset('image/partner4.png') }}" alt="Partner 4" class="img-fluid mb-3">
+            </div>
+        </div>
     </div>
 </div>
 
